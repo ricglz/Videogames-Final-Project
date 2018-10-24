7f20420b4cddb3cd8774a6f2c8b9ca52d17566f3
emifervi <emifervi@hotmail.com>
2018-10-24 17:07:49 -0500

    Added Rythm based game

---
+++ Assets/Scripts/Activator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour {
+
+	SpriteRenderer sr;
+	public KeyCode key;
+	bool active = false;
+	GameObject note, gm;
+	Color old;
+	
+	
+	void Awake(){
+		sr = GetComponent<SpriteRenderer>();
+	}
+
+	void Start(){
+		old = sr.color;
+		gm = GameObject.Find("GameManager");
+		PlayerPrefs.SetInt("Score", 0);
+	}
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(key)){
+			sr.color = new Color(0.2169811f,0.2169811f,0.2169811f);
+		}
+		if(Input.GetKeyUp(key)){
+			sr.color = old;
+		}
+		if(Input.GetKeyDown(key) && active){
+			Destroy(note);
+			gm.GetComponent<GameManager>().AddStreak();
+			AddScore();
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		active = true;
+		if(col.gameObject.CompareTag("note")){
+			note = col.gameObject;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		active = false;
+	}
+
+	void AddScore(){
+		PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score") + gm.GetComponent<GameManager>().GetScore());
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	int multiplier = 1;
+	int streak = 0;
+	int misses = 0;
+	int hitNotes = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		Destroy(col.gameObject);
+		ResetStreak();
+		misses ++;
+		if(misses >= 10){
+			Debug.Log("QUIT");
+			Application.Quit();
+		}
+	}
+	
+	public void AddStreak(){
+		streak ++;
+		hitNotes ++;
+		if(streak >= 30){
+			multiplier = 4;
+		}
+		else if(streak >= 15){
+			multiplier = 3;
+		}
+		else if(streak >= 7){
+			multiplier = 2;
+		}
+		else{
+			multiplier = 1;
+		}
+
+		if(hitNotes + misses == 103){
+			Debug.Log("QUIT");
+			Application.Quit();
+		}
+		UpdateGUI();
+	}
+
+	public void ResetStreak(){
+		streak = 0;
+		multiplier = 1;
+		UpdateGUI();
+	}
+
+	void UpdateGUI(){
+		PlayerPrefs.SetInt("Streak", streak);
+		PlayerPrefs.SetInt("Mult", multiplier);
+	}
+
+	public int GetScore(){
+		return 100 * multiplier;
+	}
+}
