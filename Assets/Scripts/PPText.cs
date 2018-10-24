1afcac70abfe706a9c945dfadffe0c1fbb5aed4b
Ricardo J Gonzalez <rjgcastillo@hotmail.com>
2018-10-24 17:27:15 -0500

    Revert "Pushed ki typing game"

---
+++ Assets/Scripts/PPText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PPText : MonoBehaviour {
+
+	public string name;
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Text>().text = PlayerPrefs.GetInt(name) + "";
+	}
+}
